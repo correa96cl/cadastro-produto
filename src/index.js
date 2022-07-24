ccb9827a174daac491941bb5f8b05509d7b2b557
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2022-07-24 09:00:01 -0400

    Commit de hoy

--- src/index.js
+++ src/index.js
@@ -5,17 +5,34 @@ const { v4: uuidv4 } = require('uuid')
 const app = express();
 const customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found" });
+    }
+    request.customer = customer;
+    return next();
+}
+
 app.use(express.json())
+
+
+
+
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
     const customerAlreadyExits = customers.some((customer) => customer.cpf === cpf);
 
-    if (customerAlreadyExits){
-        return response.status(400).json({error: "Customer already exits!"});
+    if (customerAlreadyExits) {
+        return response.status(400).json({ error: "Customer already exits!" });
     }
-  
+
 
     customers.push({
         cpf, name, id: uuidv4(), statement: []
@@ -23,7 +40,37 @@ app.post("/account", (request, response) => {
 
     return response.status(201).send()
 
-})
+});
+
+//app.use(verifyIfExistsAccountCPF);
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+const {description, amount} = request.body;
+
+const {customer} = request;
+
+const statementOperation = {
+    description,
+    amount,
+    create_at: new Date(),
+    type: "credit"
+}
+
+customer.statement.push(statementOperation);
+
+return response.status(201).send();
+
+
+
+    
+
+});
 
 app.listen(
     4000
